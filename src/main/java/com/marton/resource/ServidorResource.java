f18d57ad160b6eb383f719a32fda360f13142314
Marton Padilha <outroswp@gmail.com>
2019-03-27 21:52:58 -0300

    Commit

--- src/main/java/com/marton/resource/ServidorResource.java
+++ src/main/java/com/marton/resource/ServidorResource.java
@@ -8,16 +8,20 @@ import java.util.stream.Collectors;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marton.DTO.ServidorDTO;
+import com.marton.DTO.ServidorDTO;
+import com.marton.entity.ServidorEntity;
 import com.marton.entity.ServidorEntity;
 import com.marton.service.ServidorService;
 
@@ -53,6 +57,29 @@ public class ServidorResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> atualizar(@RequestBody ServidorEntity obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.atualizar(obj);
+		return ResponseEntity.noContent().build();//retorna 204 no servidor
+	}
+	
+	
+	@RequestMapping(value = "/paginacao", method = RequestMethod.GET)
+	public ResponseEntity<Page<ServidorDTO>> listarPaginas(
+			@RequestParam(value = "pagina", defaultValue = "0") Integer pagina, 
+			@RequestParam(value = "qtd", defaultValue = "15") Integer qtdLinhas, 
+			@RequestParam(value = "ordem", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "dir", defaultValue = "DESC") String dir){
+		
+		Page<ServidorEntity> listaCursos = 
+				service.buscarPorPagina(pagina, qtdLinhas, orderBy, dir);
+		
+		Page<ServidorDTO> listaDTO = listaCursos.map(obj -> new ServidorDTO(obj));
+		
+		return ResponseEntity.ok().body(listaDTO);
 	}
 	
 	
--- src/main/java/com/marton/service/ServidorService.java
+++ src/main/java/com/marton/service/ServidorService.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.marton.dao.ServidorDAO;
+import com.marton.entity.CursoEntity;
+import com.marton.entity.ServidorEntity;
 import com.marton.entity.ServidorEntity;
 import com.marton.exceptions.ObjNaoEncontradoException;
 
@@ -29,6 +34,28 @@ public class ServidorService {
 	public ServidorEntity salvar(ServidorEntity obj) {
 		obj.setId(null);
 		return dao.save(obj);
+	}
+	
+	public ServidorEntity atualizar(ServidorEntity obj) {
+		ServidorEntity servidor_banco = buscar(obj.getId());
+		if (obj.getNome() == null) {
+			obj.setNome(servidor_banco.getNome());
+		}
+		if (obj.getEmail() == null) {
+			obj.setEmail(servidor_banco.getEmail());
+		}
+		if (obj.getSenha() == null) {
+			obj.setSenha(servidor_banco.getSenha());
+		}
+		
+		return dao.save(obj);
+		
+	}
+	
+	public Page<ServidorEntity> buscarPorPagina(Integer pagina, Integer qtdLinhas, String orderBy, String dir){
+		PageRequest pageRequest = PageRequest.of(pagina, qtdLinhas, Direction.valueOf(dir), orderBy);
+		
+		return dao.findAll(pageRequest);
 	}
 	
 }
